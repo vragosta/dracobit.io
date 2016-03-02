94545e3aaa2d42a54aefdf370dd9bb303c4fe678
Vincent Ragosta <vragosta@storycorps.org>
2016-03-02 11:56:41 -0500

    combined left and right columns, removed fixed css property temporatily, adjusted templates

--- themes/dracobit-twentysixteen/page-tutorials.php
+++ themes/dracobit-twentysixteen/page-tutorials.php
@@ -7,36 +7,7 @@
 
 <div id="tutorials-page-container" class="container">
 	<div class="row">
-		<nav id="tutorials-nav-menu" class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-			<legend>Tutorials</legend>
-			<div class="list-group">
-
-			<?php if ( $tutorials->have_posts() ) {
-				while ( $tutorials->have_posts() ) : $tutorials->the_post(); ?>
-
-					<a href="#tutorial-<?php echo $post->post_name; ?>" class="list-group-item" name="<?php echo $post->post_name; ?>" data-tab="<?php echo $tutorials->current_post; ?>">
-						<div class="row">
-							<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
-								<h4 class="list-group-item-heading"><?php the_title(); ?></h4>
-							</div>
-							<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-								<p id="tutorials-version">v<?php echo get_post_meta( $post->ID, 'version', true); ?></p>
-							</div>
-						</div>
-
-						<div class="row">
-							<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-								<p class="list-group-item-text"><?php echo get_post_meta( $post->ID, 'tagline', true ); ?></p>
-							</div>
-						</div>
-					</a>
-
-					<?php endwhile;
-				}
-			?>
-			</div>
-		</nav>
-		<main id="tutorials-content" class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+		<main id="tutorials-content" class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
 			<?php
 				if ( $tutorials->have_posts() ) {
 					while ( $tutorials->have_posts() ) : $tutorials->the_post(); ?>
