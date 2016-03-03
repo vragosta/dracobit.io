70cb64c580e4151c6ae2875398170e348c603b9a
Frankie Bordone <frankieb132@gmail.com>
2016-03-02 21:09:00 -0500

    Implemented tab functionality to switch between tutorials (found bug, stupid #)

--- themes/dracobit-twentysixteen/js/src/dracobit.js
+++ themes/dracobit-twentysixteen/js/src/dracobit.js
@@ -2,29 +2,10 @@ $(function() {
 
 	$( 'a.list-group-item' ).click(function() {
 		$name = $( this ).attr( 'name' );
+		$container = $( '#tutorials-' + $name + '-container' );
 
-		if ( 'linux-for-beginners' == $name ) {
-			$( '#tutorials-css-for-beginners-container' ).hide();
-			$( '#tutorials-github-for-beginners-container' ).hide();
-			$( '#tutorials-linux-for-beginners-container' ).show();
-		} else if ( 'css-for-beginners' == $name ) {
-			$( '#tutorials-linux-for-beginners-container' ).hide();
-			$( '#tutorials-github-for-beginners-container' ).hide();
-			$( '#tutorials-css-for-beginners-container' ).show();
-		} else if ( 'github-for-beginners' == $name ) {
-			$( '#tutorials-linux-for-beginners-container' ).hide();
-			$( '#tutorials-css-for-beginners-container' ).hide();
-			$( '#tutorials-github-for-beginners-container' ).show();
-		}
+		$container.siblings().css( 'display', 'none' );
+		$container.show();
+
 	});
-
 });
-
-// $name = $( this ).attr( 'name' );
-// $container = $( 'tutorials-' + $name + '-container' );
-//
-// $container.siblings().css( 'display', 'none' );
-//
-// console.log( $container.siblings() );
-//
-// $container.show();
