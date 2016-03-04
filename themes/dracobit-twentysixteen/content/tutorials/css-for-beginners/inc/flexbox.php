6ede74921870d80abbd85ee2cdfb3a00581f3594
Frankie Bordone <frankieb132@gmail.com>
2016-03-03 19:52:38 -0500

    encapsulated all <i> tags with <p> tags

--- themes/dracobit-twentysixteen/content/tutorials/css-for-beginners/inc/flexbox.php
+++ themes/dracobit-twentysixteen/content/tutorials/css-for-beginners/inc/flexbox.php
@@ -46,7 +46,7 @@
 				<p><code>display: -webkit-flex;</code></p>
 			</div>
 			<div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
 			<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
       	<p>apply to container div; the content and children of that div now instantly become flex items.</p>
@@ -58,7 +58,7 @@
 				<p><code>-webkit-flex-direction: </code></p>
 			</div>
 			<div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
 			<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
 				<p>controls the direction of the flex items. (<strong>Possible Parameters: row | row-reverse | column | column-reverse</strong>)</p>
@@ -70,7 +70,7 @@
 				<p><code>-webkit-justify-content: </code></p>
 			</div>
 			<div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
 			<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
 				<p>defines the alignment along the main axis. (<strong>Possible Parameters: flex-start | flex-end | center | space-between | space-around</strong>)</p>
@@ -82,7 +82,7 @@
 				<p><code>-webkit-flex-wrap: </code></p>
 			</div>
 			<div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
 			<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
 				<p>flex items will all try to fit onto one line by default. You can change that and allow the items to wrap as needed with this property. (<strong>Possible Parameters: nowrap | wrap | wrap-reverse</strong>)</p>
@@ -94,7 +94,7 @@
 				<p><code>-webkit-flex-grow: </code></p>
 			</div>
 			<div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
 			<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
 				<p>accepts a unitless value that serves as a proportion. It dictates what amount of the available space inside the flex container the item should take up. (<strong>Possible Parameters: < number ></strong>)</p>
@@ -106,7 +106,7 @@
 				<p><code>-webkit-order: </code></p>
 			</div>
 			<div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
 			<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
 				<p>flex items are laid out in the source order by default. However, the order property controls the order in which they appear in the flex container. (<strong>Possible Parameters: < number >; </strong>)</p>
@@ -118,7 +118,7 @@
 				<p><code>-webkit-align-self: </code></p>
 			</div>
 			<div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
 			<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
 				<p>allows the default alignment (or the one specified by align-items) to be overridden for individual flex items. (<strong>Possible Parameters: auto | flex-start | flex-end | center | baseline | stretch</strong>)</p>
@@ -130,7 +130,7 @@
 				<p><code>-webkit-align-items: </code></p>
 			</div>
 			<div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
 			<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
 				<p>defines the default behaviour for how flex items are laid out along the cross axis on the current line. Think of it as the justify-content version for the cross-axis (perpendicular to the main-axis). (<strong>Possible Parameters: flex-start | flex-end | center | baseline | stretch</strong>)</p>
@@ -142,7 +142,7 @@
 				<p><code>-webkit-align-content: </code></p>
 			</div>
 			<div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
 			<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
 				<p>aligns a flex container's lines within when there is extra space in the cross-axis, similar to how justify-content aligns individual items within the main-axis. (<strong>Possible Parameters: flex-start | flex-end | center | space-between | space-around | stretch</strong>)</p>
@@ -154,7 +154,7 @@
 				<p><code>-webkit-column-count: </code></p>
 			</div>
 			<div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
 			<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
 				<p>determines margin between columns. (<strong>Possible Parameters: < number ></strong>)</p>
@@ -166,7 +166,7 @@
 				<p><code>-webkit-column-gap: </code></p>
 			</div>
 			<div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
 			<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
 				<p>apply to container div; the content and children of that div now instantly become flex items. (<strong>Possible Parameters: < number ></strong>)</p>
@@ -178,7 +178,7 @@
 				<p><code>-webkit-column-rule: </code></p>
 			</div>
 			<div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
 			<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
 				<p>creates vertical lines in gaps of columns. (<strong>Possible Parameters Example: 2px dotted #FFF</strong>)</p>
--- themes/dracobit-twentysixteen/content/tutorials/github-for-beginners/inc/github-basics.php
+++ themes/dracobit-twentysixteen/content/tutorials/github-for-beginners/inc/github-basics.php
@@ -10,7 +10,7 @@
         <p><code>$ git pull</code></p>
       </div>
       <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
       <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
         <p>Pulls down bookmark history and incorporates the changes into your repository.</p>
@@ -22,7 +22,7 @@
         <p><code>$ git status</code></p>
       </div>
       <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
       <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
         <p>Lists all new or modified files to be commited.</p>
@@ -34,7 +34,7 @@
         <p><code>$ git add [file]</code></p>
       </div>
       <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
       <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
         <p>Adds the files in the local repository and stages them for commit.</p>
@@ -46,7 +46,7 @@
         <p><code>$ git commit -m ""</code></p>
       </div>
       <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-        <i class="fa fa-long-arrow-right"></i>
+        <p><i class="fa fa-long-arrow-right"></i></p>
       </div>
       <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
         <p>Commits the tracked changes and prepares them to be pushed to a remote repository.</p>
@@ -58,7 +58,7 @@
         <p><code>$ git push</code></p>
       </div>
       <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
       <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
         <p>Pushes the changes in your local repository up to the remote repository you specified as the origin.</p>
@@ -70,7 +70,7 @@
         <p><code>$ git diff</code></p>
       </div>
       <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+        <p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
       <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
         <p>Shows file differences not yet staged</p>
@@ -82,7 +82,7 @@
         <p><code>$ git reset [file]</code></p>
       </div>
       <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
       <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
         <p>Unstages the file, but preserve its contents.</p>
@@ -94,7 +94,7 @@
         <p><code>$ git checkout</code></p>
       </div>
       <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-				<i class="fa fa-long-arrow-right"></i>
+				<p><i class="fa fa-long-arrow-right"></i></p>
 			</div>
       <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
         <p>Switches to the specified branch and updates the working directory.</p>
@@ -108,7 +108,7 @@
 
     <div class="row">
       <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
-        <i class="fa fa-arrow-right"></i>
+        <p><i class="fa fa-arrow-right"></i></p>
       </div>
       <div class="col-xs-11 col-sm-11 col-md-11 col-lg-11">
         <p>Every GitHub repository comes equipped with a section for hosting documentation, called a Wiki. See <a href="https://help.github.com/articles/about-github-wikis/"target="_blank">this link</a> for full details about GitHub Wikis.</p>
