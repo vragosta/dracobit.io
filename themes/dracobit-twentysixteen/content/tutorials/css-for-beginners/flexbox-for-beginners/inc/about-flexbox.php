d1ede46ca331606b3c20059d45593813043ac9af
Frankie Bordone <frankieb132@gmail.com>
2016-03-04 19:05:02 -0500

    changed file directory name for css-for-beginners

---
+++ themes/dracobit-twentysixteen/content/tutorials/css-for-beginners/flexbox-for-beginners/inc/about-flexbox.php
@@ -0,0 +1,41 @@
+<a id="tutorial-about-flexbox-anchor" class="anchor"></a>
+<section id="tutorial-about-flexbox">
+	<legend>Flexbox</legend>
+
+	<article id="tutorial-why-flexbox">
+			<h4>What is Flexbox and why is it useful?</h4>
+
+			<div class="row">
+				<div class="col-xs-11 col-sm-11 col-md-11 col-lg-11">
+					<p>Flexible boxes, or flexbox, is a new layout mode in CSS3.</p>
+					<p>Use of flexbox ensures that elements behave predictably when the page layout must accommodate different screen sizes and different display devices.</p>
+					<p>For many applications, the flexible box model provides an improvement over the block model in that it does not use floats, nor do the flex container's margins collapse with the margins of its contents.</p>
+				</div>
+			</div>
+
+	</article>
+
+	<article id="tutorial-flexbox-compatibility">
+		<h4>Is Flexbox compatible to my web browser?</h4>
+
+		<div class="row">
+			<div class="col-xs-11 col-sm-11 col-md-11 col-lg-11">
+				<p>Check to see if your browser is compatible <a href="http://caniuse.com/#feat=flexbox" target="_blank">here</a>.</p>
+			</div>
+		</div>
+
+	</article>
+
+	<article id="tutorial-flexbox-basics">
+		<h4>Flexbox Basics</h4>
+
+		<div class="row">
+			<div class="col-xs-11 col-sm-11 col-md-11 col-lg-11">
+				<p>Flexbox consists of <strong>flex containers</strong> and <strong>flex items</strong>. Flex items align with respect to their flex container or with respect to eachother. </p>
+				<p>Flexbox defines how flex items are laid out inside flex container. Flex items by default go left to right and top to bottom. Flexbox has a concept of 2 axis: the <strong>main axis</strong> (left to right) and the <strong>cross axis</strong> (top to bottom). These axis determine the baseline of the flex items. </p>
+			</div>
+		</div>
+
+	</article>
+
+</section>
