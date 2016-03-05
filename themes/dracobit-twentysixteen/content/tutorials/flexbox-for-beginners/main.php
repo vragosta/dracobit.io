c2b1edadd9b4680a343831166c9bbae54f3497fc
Frankie Bordone <frankieb132@gmail.com>
2016-03-05 15:01:23 -0500

    fixed the file structure for Flexbox tutorial, cleaned up some code

---
+++ themes/dracobit-twentysixteen/content/tutorials/flexbox-for-beginners/main.php
@@ -0,0 +1,10 @@
+<section id="tutorial-header-container">
+	<legend>Flexbox for Beginners</legend>
+	<p>A look into Flexbox and it's properties</p>
+</section>
+
+<?php
+	include_once get_template_directory() . '/content/tutorials/flexbox-for-beginners/inc/about-flexbox.php';
+
+	include_once get_template_directory() . '/content/tutorials/flexbox-for-beginners/inc/flexbox-notable-properties.php'
+?>
