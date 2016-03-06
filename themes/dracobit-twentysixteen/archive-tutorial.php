f0952a53b6e5ce4531c873b6bae247fd2f99f6ab
Vincent Ragosta <allchenzo@gmail.com>
2016-03-06 10:36:13 -0500

    implemented fail-safes for tagline and version, and added a float: right to the version display on frontend

--- themes/dracobit-twentysixteen/archive-tutorial.php
+++ themes/dracobit-twentysixteen/archive-tutorial.php
@@ -16,13 +16,31 @@
 									<h4 class="list-group-item-heading"><?php the_title(); ?></h4>
 								</div>
 								<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-									<p id="tutorial-version">v<?php echo get_post_meta( $post->ID, 'version', true); ?></p>
+									<p id="tutorial-version">
+										<?php
+											$version = get_post_meta( $post->ID, 'version', true);
+											if ( $version ) {
+												echo 'v' . $version;
+											} else {
+												echo 'v1.0';
+											}
+										?>
+									</p>
 								</div>
 							</div>
 
 							<div class="row">
 								<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-									<p class="list-group-item-text"><?php echo get_post_meta( $post->ID, 'tagline', true ); ?></p>
+									<p class="list-group-item-text">
+										<?php
+											$tagline =  get_post_meta( $post->ID, 'tagline', true );
+											if ( $tagline ) {
+												echo $tagline;
+											} else {
+												echo 'Please enter tagline.';
+											}
+										?>
+									</p>
 								</div>
 							</div>
 						</a>
