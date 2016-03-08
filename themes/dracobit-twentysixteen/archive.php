6bcc93333c86e2ec1de35c5475f3bde075b58f2c
Vincent Ragosta <vragosta@storycorps.org>
2016-03-08 11:55:41 -0500

    adjusted archive/single templates and added some styles to the aside element

--- themes/dracobit-twentysixteen/archive.php
+++ themes/dracobit-twentysixteen/archive.php
@@ -3,7 +3,7 @@
 ?>
 
 <div class="tutorial-page-container container">
-	<main id="tutorial-nav" class="row">
+	<main class="row">
 		<section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
 			<legend>Tutorials Archive</legend>
 			<div class="tutorial-section">
--- themes/dracobit-twentysixteen/single.php
+++ themes/dracobit-twentysixteen/single.php
@@ -3,7 +3,7 @@
 ?>
 
 <div class="tutorial-page-container container">
-	<main id="tutorial-nav" class="row">
+	<main class="row">
 		<section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
 			<div class="tutorial-section">
 				<?php get_template_part( 'content', get_post_type() ); ?>
