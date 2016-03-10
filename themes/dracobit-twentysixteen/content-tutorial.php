acd5191dcaa0e1c02f6d5f22cd78e53460ea1115
Vincent Ragosta <vragosta@storycorps.org>
2016-03-10 15:10:48 -0500

    removed the title and tagline from the linux tutorial and embedded it into the tutorial single template, began working on conforming the styles to match the post archive/single

--- themes/dracobit-twentysixteen/content-tutorial.php
+++ themes/dracobit-twentysixteen/content-tutorial.php
@@ -1,8 +1,27 @@
 <script class="tutorial" type="html/template">
-	<section id="tutorial-content" class="row">
-		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-			<div id="tutorial-<%= tutorial.slug %>-container" class="tutorial-container">
-				<%= tutorial.content %>
+	<section class="tutorial-container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div class="tutorial-title">
+					<legend>
+						<%= ( tutorial.title ) ? tutorial.title : '' %>
+						<span class="tutorial-version"><%= ( tutorial.version ) ? 'v' + tutorial.version : 'v1.0' %></span>
+					</legend>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div class="tutorial-tagline">
+					<%= ( tutorial.tagline ) ? tutorial.tagline : '' %>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div class="tutorial-content">
+					<%= ( tutorial.content ) ? tutorial.content : '' %>
+				</div>
 			</div>
 		</div>
 	</section>
@@ -11,7 +30,7 @@
 <script class="tutorials" type="html/template">
 	<% _.each( tutorials, function( tutorial ) { %>
 		<div class="list-group">
-			<a href="/tutorial/<%= tutorial.slug %>" class="list-group-item" name="<%= tutorial.slug %>">
+			<a href="/tutorial/<%= tutorial.slug %>" class="list-group-item archive-item" name="<%= tutorial.slug %>">
 				<div class="row">
 					<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 						<h4 class="list-group-item-heading">
@@ -23,7 +42,7 @@
 						</h4>
 					</div>
 					<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-						<p id="tutorial-version">
+						<p class="tutorial-version">
 							<% if ( tutorial.version ) { %>
 								<%= 'v' + tutorial.version %>
 							<% } else { %>
--- themes/dracobit-twentysixteen/content/tutorials/linux-for-beginners/main.php
+++ themes/dracobit-twentysixteen/content/tutorials/linux-for-beginners/main.php
@@ -1,17 +1,14 @@
 <section id="tutorial-header-container">
-  <legend>Linux for Beginners</legend>
-  <p>An introduction to the Linux Operating System and the Command Line Interface.</p>
+  <?php
+  	include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/preparing-and-installing-ubuntu.php';
+  	include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/installing-guest-additions.php';
+  	include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/getting-connected-via-ssh.php';
+  	include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/linux-directory-structure.php';
+  	include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/welcome-to-shell.php';
+  	include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/basic-linux-commands.php';
+  	include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/working-with-directories.php';
+  	include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/listing-files-and-understanding-ls-output.php';
+    include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/file-and-directory-permissions-explained.php';
+    include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/finding-files-and-directories.php';
+  ?>
 </section>
-
-<?php
-	include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/preparing-and-installing-ubuntu.php';
-	include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/installing-guest-additions.php';
-	include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/getting-connected-via-ssh.php';
-	include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/linux-directory-structure.php';
-	include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/welcome-to-shell.php';
-	include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/basic-linux-commands.php';
-	include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/working-with-directories.php';
-	include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/listing-files-and-understanding-ls-output.php';
-  include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/file-and-directory-permissions-explained.php';
-  include_once get_template_directory() . '/content/tutorials/linux-for-beginners/inc/finding-files-and-directories.php';
-?>
