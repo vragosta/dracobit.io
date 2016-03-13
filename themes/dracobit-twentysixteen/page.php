4e99a9ec820b7c0642f5536d88c0ae559a3d5c34
Vincent Ragosta <allchenzo@gmail.com>
2016-03-12 20:21:07 -0500

    removed login and signup templates, constructed basic profile template

--- themes/dracobit-twentysixteen/page.php
+++ themes/dracobit-twentysixteen/page.php
@@ -2,14 +2,23 @@
 
 <div class="dracobit-container container">
 	<main class="row">
-		<section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
-			<legend>Blog</legend>
-			<div class="<?php echo get_post_type(); ?>-section">
-				<?php get_template_part( 'content', get_post_type() ); ?>
-			</div>
-		</section>
-		<?php get_sidebar( get_post_type() ); ?>
-	</main>
+    <section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+			<legend>Profile</legend>
+      <!-- <div id="profile-user-display" class="row">
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div id="profile-avatar-container">
+            <?php echo get_avatar( $current_user->ID, 150 ); ?>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div id="profile-rank-display">
+
+          </div>
+        </div>
+      </div> -->
+    </section>
+    <?php get_sidebar( get_post_type() ); ?>
+  </main>
 </div>
 
 <?php get_footer(); ?>
