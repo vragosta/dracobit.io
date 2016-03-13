b2d3d9e0b529d736a5f3acb995905e50ecb5d66c
Vincent Ragosta <allchenzo@gmail.com>
2016-03-13 13:36:52 -0400

    set up workspace to begin constructing profile wall

--- themes/dracobit-twentysixteen/page-profile.php
+++ themes/dracobit-twentysixteen/page-profile.php
@@ -3,7 +3,10 @@
 <div class="dracobit-container container">
 	<main class="row">
     <section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
-			<!-- fill this out once profile template is complete -->
+			<legend>Profile</legend>
+			<?php $comments = get_comments( array( 'post_id' => $post->ID ) ); ?>
+			<?php wp_list_comments( array( 'callback' => 'mytheme_comment' ), $comments ); ?>
+			<?php comment_form( array( 'title_reply_to' => '' ) ); ?>
     </section>
     <?php get_sidebar( get_post_type() ); ?>
   </main>
