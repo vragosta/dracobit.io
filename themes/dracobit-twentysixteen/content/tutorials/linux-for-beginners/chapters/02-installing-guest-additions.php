b2780b7085ff433d404d6a7e9e8316f6ff3a44b0
Vincent Ragosta <allchenzo@gmail.com>
2016-04-08 21:20:25 -0400

    renamed files for better readability

---
+++ themes/dracobit-twentysixteen/content/tutorials/linux-for-beginners/chapters/02-installing-guest-additions.php
@@ -0,0 +1,47 @@
+<section id="tutorial-installing-guest-additions">
+  <legend>Installing Guest Additions</legend>
+
+  <article id="tutorial-installing-guest-additions-on-ubuntu">
+    <h4>Installing Guest Additions on Ubuntu</h4>
+    <div class="row">
+      <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+        <p><i class="fa fa-arrow-right"></i></p>
+      </div>
+      <div class="col-xs-11 col-sm-11 col-md-11 col-lg-11">
+        <p>Power up the virtual machine, login with the credentials provided during installation.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+        <p><i class="fa fa-arrow-right"></i></p>
+      </div>
+      <div class="col-xs-11 col-sm-11 col-md-11 col-lg-11">
+        <p>Select from the top menu bar of the virtual machine <strong>Devices->Insert Guest Additions CD Image</strong>.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+        <p><i class="fa fa-arrow-right"></i></p>
+      </div>
+      <div class="col-xs-11 col-sm-11 col-md-11 col-lg-11">
+        <p>Select <strong>Run</strong> from the prompt, enter password and let the virtualbox guest additions install.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+        <p><i class="fa fa-arrow-right"></i></p>
+      </div>
+      <div class="col-xs-11 col-sm-11 col-md-11 col-lg-11">
+        <p><strong>Final Step:</strong> Restart or powerdown the virtual machine to update the system.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+        <p><i class="fa fa-arrow-right"></i></p>
+      </div>
+      <div class="col-xs-11 col-sm-11 col-md-11 col-lg-11">
+        <p>Congratulations, you have successfully installed Ubuntu on Oracle Virtualbox!<p>
+      </div>
+    </div>
+  </article>
+</section>
