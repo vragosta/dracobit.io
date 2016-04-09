00f0cc7175a6602baf450926f384a6efd80bcb82
Vincent Ragosta <allchenzo@gmail.com>
2016-04-09 13:44:11 -0400

    finished HTML cleanup

--- themes/dracobit-twentysixteen/content/tutorials/linux-for-beginners/chapters/06-basic-linux-commands.php
+++ themes/dracobit-twentysixteen/content/tutorials/linux-for-beginners/chapters/06-basic-linux-commands.php
@@ -1,100 +1,102 @@
-<section id="tutorial-basic-linux-commands">
+<section id="tutorial-basic-commands">
   <legend>Basic Linux Commands</legend>
 
-  <div class="row">
-    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-      <p><code>ls</code></p>
+  <article id="tutorial-basic-linux-commands">
+    <h4>Basic Linux Commands</h4>
+    <div class="row">
+      <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+        <p><code>ls</code></p>
+      </div>
+      <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
+        <p><i class="fa fa-long-arrow-right"></i></p>
+      </div>
+      <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+        <p>Lists directory contents.</p>
+      </div>
     </div>
-    <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-      <p><i class="fa fa-long-arrow-right"></i></p>
-    </div>
-    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-      <p>Lists directory contents.</p>
-    </div>
-  </div>
 
-  <div class="row">
-    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-      <p><code>cd</code></p>
+    <div class="row">
+      <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+        <p><code>cd</code></p>
+      </div>
+      <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
+        <p><i class="fa fa-long-arrow-right"></i></p>
+      </div>
+      <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+        <p>Changes the current directory.</p>
+      </div>
     </div>
-    <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-      <p><i class="fa fa-long-arrow-right"></i></p>
-    </div>
-    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-      <p>Changes the current directory.</p>
-    </div>
-  </div>
 
-  <div class="row">
-    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-      <p><code>pwd</code></p>
+    <div class="row">
+      <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+        <p><code>pwd</code></p>
+      </div>
+      <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
+        <p><i class="fa fa-long-arrow-right"></i></p>
+      </div>
+      <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+        <p>Displays the present working directory.</p>
+      </div>
     </div>
-    <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-      <p><i class="fa fa-long-arrow-right"></i></p>
-    </div>
-    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-      <p>Displays the present working directory.</p>
-    </div>
-  </div>
 
-  <div class="row">
-    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-      <p><code>cat</code></p>
+    <div class="row">
+      <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+        <p><code>cat</code></p>
+      </div>
+      <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
+        <p><i class="fa fa-long-arrow-right"></i></p>
+      </div>
+      <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+        <p>Concatenates and displays files.</p>
+      </div>
     </div>
-    <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-      <p><i class="fa fa-long-arrow-right"></i></p>
-    </div>
-    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-      <p>Concatenates and displays files.</p>
-    </div>
-  </div>
 
-  <div class="row">
-    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-      <p><code>echo</code></p>
+    <div class="row">
+      <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+        <p><code>echo</code></p>
+      </div>
+      <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
+        <p><i class="fa fa-long-arrow-right"></i></p>
+      </div>
+      <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+        <p>Displays arguments to the screen.</p>
+      </div>
     </div>
-    <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-      <p><i class="fa fa-long-arrow-right"></i></p>
-    </div>
-    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-      <p>Displays arguments to the screen.</p>
-    </div>
-  </div>
 
-  <div class="row">
-    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-      <p><code>man</code></p>
+    <div class="row">
+      <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+        <p><code>man</code></p>
+      </div>
+      <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
+        <p><i class="fa fa-long-arrow-right"></i></p>
+      </div>
+      <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+        <p>Displays the online manual.</p>
+      </div>
     </div>
-    <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-      <p><i class="fa fa-long-arrow-right"></i></p>
-    </div>
-    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-      <p>Displays the online manual.</p>
-    </div>
-  </div>
 
-  <div class="row">
-    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-      <p><code>exit</code></p>
+    <div class="row">
+      <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+        <p><code>exit</code></p>
+      </div>
+      <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
+        <p><i class="fa fa-long-arrow-right"></i></p>
+      </div>
+      <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+        <p>Exits the shell or your current session.</p>
+      </div>
     </div>
-    <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-      <p><i class="fa fa-long-arrow-right"></i></p>
-    </div>
-    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-      <p>Exits the shell or your current session.</p>
-    </div>
-  </div>
 
-  <div class="row">
-    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-      <p><code>clear</code></p>
+    <div class="row">
+      <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+        <p><code>clear</code></p>
+      </div>
+      <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
+        <p><i class="fa fa-long-arrow-right"></i></p>
+      </div>
+      <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+        <p>Clears the screen.</p>
+      </div>
     </div>
-    <div class="col-xs-1 xol-sm-1 col-md-1 col-lg-1">
-      <p><i class="fa fa-long-arrow-right"></i></p>
-    </div>
-    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-      <p>Clears the screen.</p>
-    </div>
-  </div>
-
+  </article>
 </section>
