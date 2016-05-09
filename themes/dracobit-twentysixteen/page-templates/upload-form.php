d461ead8dd29f8c09cad21c6f6c6dad698e9b9e7
Vincent Ragosta <allchenzo@gmail.com>
2016-05-09 00:41:31 -0400

    retemplated and styled the upload form

--- themes/dracobit-twentysixteen/page-templates/upload-form.php
+++ themes/dracobit-twentysixteen/page-templates/upload-form.php
@@ -8,95 +8,95 @@ get_header(); ?>
 
 <div class="dracobit-container container">
 	<main class="row">
-    <section class="col-xs-12 col-sm-8 col-md-8 col-lg-8" style="display: flex; flex-direction: column; height: 650px; justify-content: space-between;">
+    <section class="col-xs-12 col-sm-8 col-md-8 col-lg-8 upload-section-container">
 
-			<div class="row" style="height: 50px;">
-				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="display: flex; align-items: center; justify-content: space-between;">
-					<h3 style="margin: 10px 0;">Add Chapter</h3>
-					<a href="<?php echo home_url( 'profile' ); ?>" style="color: #ccc; text-decoration: none;">Back to profile</a>
+			<div class="row upload-header-container">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<h3>Add Chapter</h3>
+					<a href="<?php echo home_url( 'profile' ); ?>">Back to profile</a>
 				</div>
 			</div>
 
-			<div class="row upload-message-container" style="display: none;">
+			<div class="row upload-message-container">
 				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-					<p class=" alert-danger upload-message" style="margin: 0; padding: 5px; border: 1px solid #ebccd1;">Uploading chapter... this may take a few minutes.</p>
+					<p class=" alert-danger upload-message">Uploading chapter... this may take a few minutes.</p>
 					<div class="row">
-						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="padding-right: 0;">
-							<div class="progress progress-striped active" style="display: none;">
-								<div class="bar" style="width: 0%;"></div>
+						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+							<div class="progress progress-striped active">
+								<div class="bar"></div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 
-			<div class="row-fluid">
-				<div class="row" style="height: 50px;">
-					<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" style="display: flex; align-items: center; height: 100%">
+			<div class="row">
+				<div class="row upload-chapter">
+					<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 upload-step-container">
 						<div class="upload-step">1</div>
 					</div>
-					<div class="col-xs-11 col-sm-11 col-md-11 col-lg-11" style="display: flex; align-items: center; height: 100%; padding: 0;">
+					<div class="col-xs-11 col-sm-11 col-md-11 col-lg-11 upload-content-text">
 						<h4>Enter Chapter title.</h4>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xs-11 col-sm-11 col-md-11 col-lg-11" style="padding: 0;">
-						<textarea type="text" class="form-control upload-chapter-title" style="border-radius: 0; resize: none; height: 70px;" /></textarea>
-					</div>
-				</div>
-			</div>
-
-			<div class="row-fluid">
-				<div class="row" style="height: 50px;">
-					<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" style="display: flex; align-items: center; height: 100%">
-						<div class="upload-step">2</div>
-					</div>
-					<div class="col-xs-11 col-sm-11 col-md-11 col-lg-11" style="display: flex; align-items: center; height: 100%; padding: 0;">
-						<h4>Enter Chapter Content.</h4>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xs-11 col-sm-11 col-md-11 col-lg-11" style="padding: 0;">
-						<textarea type="text" class="form-control upload-chapter-content" style="border-radius: 0; resize: none; height: 100px;" /></textarea>
-					</div>
-				</div>
-			</div>
-
-			<div class="row-fluid">
-				<div class="row" style="height: 50px;">
-					<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" style="display: flex; align-items: center; height: 100%">
-						<div class="upload-step">3</div>
-					</div>
-					<div class="col-xs-11 col-sm-11 col-md-11 col-lg-11" style="display: flex; align-items: center; height: 100%; padding: 0;">
-						<h4>Enter Chapter Tagline.</h4>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xs-11 col-sm-11 col-md-11 col-lg-11" style="padding: 0;">
-						<textarea type="text" class="form-control upload-chapter-tagline" style="border-radius: 0; resize: none; height: 70px;" /></textarea>
-					</div>
-				</div>
-			</div>
-
-			<div class="row-fluid">
-				<div class="row" style="height: 50px;">
-					<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" style="display: flex; align-items: center; height: 100%">
-						<div class="upload-step">4</div>
-					</div>
-					<div class="col-xs-11 col-sm-11 col-md-11 col-lg-11" style="display: flex; align-items: center; height: 100%; padding: 0;">
-						<h4>Enter Chapter Version.</h4>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xs-11 col-sm-11 col-md-11 col-lg-11" style="padding: 0;">
-						<input type="text" class="form-control upload-chapter-version" style="border-radius: 0; resize: none;" />
+				<div class="row upload-content">
+					<div class="col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xs-11 col-sm-11 col-md-11 col-lg-11">
+						<textarea type="text" class="form-control upload-chapter-title" /></textarea>
 					</div>
 				</div>
 			</div>
 
 			<div class="row">
-				<div class="col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xs-11 col-sm-11 col-md-11 col-lg-11" style="padding: 0;">
-					<button type="button" class="btn btn-info upload-submit" style="width: 100%;">Upload Chapter</button>
+				<div class="row upload-chapter">
+					<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 upload-step-container">
+						<div class="upload-step">2</div>
+					</div>
+					<div class="col-xs-11 col-sm-11 col-md-11 col-lg-11 upload-content-text">
+						<h4>Enter Chapter Content.</h4>
+					</div>
+				</div>
+				<div class="row upload-content">
+					<div class="col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xs-11 col-sm-11 col-md-11 col-lg-11">
+						<textarea type="text" class="form-control upload-chapter-content" /></textarea>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="row upload-chapter">
+					<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 upload-step-container">
+						<div class="upload-step">3</div>
+					</div>
+					<div class="col-xs-11 col-sm-11 col-md-11 col-lg-11 upload-content-text">
+						<h4>Enter Chapter Tagline.</h4>
+					</div>
+				</div>
+				<div class="row upload-content">
+					<div class="col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xs-11 col-sm-11 col-md-11 col-lg-11">
+						<textarea type="text" class="form-control upload-chapter-tagline" /></textarea>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="row upload-chapter">
+					<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 upload-step-container">
+						<div class="upload-step">4</div>
+					</div>
+					<div class="col-xs-11 col-sm-11 col-md-11 col-lg-11 upload-content-text">
+						<h4>Enter Chapter Version.</h4>
+					</div>
+				</div>
+				<div class="row upload-content">
+					<div class="col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xs-11 col-sm-11 col-md-11 col-lg-11">
+						<input type="text" class="form-control upload-chapter-version" />
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xs-11 col-sm-11 col-md-11 col-lg-11 upload-submit-container">
+					<button type="button" class="btn btn-info upload-submit">Upload Chapter</button>
 				</div>
 			</div>
 
