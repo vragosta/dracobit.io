e4abe0c5029935ce72f435359897fab3c79a0e02
Francesco Bordone <francescobordone@Francesco-Bordone.local>
2016-05-18 17:22:12 -0400

    Implemented function that retreives milestone data from our repo's api

--- plugins/dracobit-github-issues/dracobit-github-issues.php
+++ plugins/dracobit-github-issues/dracobit-github-issues.php
@@ -13,10 +13,26 @@
  * @since      1.0.0
  */
 
+function dracobit_get_milestones( $atts ) {
+  $url = "https://api.github.com/repos/vragosta/dracobit.io/milestones";
+
+  $response = wp_remote_get( $url );
+
+  $milestones = json_decode( wp_remote_retrieve_body ( $response ) );
+
+  return $milestones;
+}
+
+function dracobit_get_issues( $atts, $milestone ) {
+  $url = "https://api.github.com/repos/vragosta/dracobit.io/issues";
+
+  $query = array();
+}
+
 function dracobit_generate_shortcode( $atts ) {
   $atts = shortcode_atts( array(
       'state'      => 'all',
-      'assignee'   => '',
+      'assignee'   => 'Frankie',
   		'creator'	   => '',
       'labels'     => '',
       'sort'     => 'updated',
@@ -24,7 +40,13 @@ function dracobit_generate_shortcode( $atts ) {
     	'repo'       => ''
   ), $atts );
 
-  return $atts;
+  $milestones = dracobit_get_milestones( $atts );
+
+  foreach( $milestones as $milestone ) {
+    $output .= $milestone->title . '<br>';
+  }
+
+  return $output;
 }
 add_shortcode( 'dracobit', 'dracobit_generate_shortcode' );
 
