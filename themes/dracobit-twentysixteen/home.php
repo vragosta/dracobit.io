114a4ddb7fb2ff656ffa3768a4a9f4273c3509e8
Vincent Ragosta <vragosta@storycorps.org>
2016-05-25 12:40:26 -0400

    testing deployments

--- themes/dracobit-twentysixteen/home.php
+++ themes/dracobit-twentysixteen/home.php
@@ -6,7 +6,7 @@
 			<legend>News Feed</legend>
 			<div class="dracobit-section">
 				<?php get_template_part( 'content', get_post_type() ); ?>
-				<?php echo 'hello this is a test for deployments'; ?>
+				<?php echo 'hello this is a test for deployments, making another change to test deployments!!'; ?>
 			</div>
 		</section>
 		<?php get_sidebar( get_post_type() ); ?>
