037962459a3e63b629c3e0b3350219a6c954923f
Frankie Bordone <frankieb132@gmail.com>
2016-06-06 20:48:24 -0400

    Modularized functions to obtain milestones and issues from Github API, or in other words a bunch of salami

--- plugins/dracobit-github-issues/dracobit-github-issues.php
+++ plugins/dracobit-github-issues/dracobit-github-issues.php
@@ -13,6 +13,24 @@
  * @since      1.0.0
  */
 
+function dracobit_get_milestones( $atts ) {
+  $url = "https://api.github.com/repos/vragosta/dracobit.io/milestones";
+
+  $response = wp_remote_get( $url );
+  $milestones = json_decode( wp_remote_retrieve_body ( $response ) );
+
+  return $milestones;
+}
+
+function dracobit_get_issues( $atts, $milestone ) {
+  $url = "https://api.github.com/repos/vragosta/dracobit.io/issues";
+
+  $response = wp_remote_get( $url );
+  $issues = json_decode( wp_remote_retrieve_body( $url ) );
+
+  return $issues;
+}
+
 function dracobit_generate_shortcode( $atts ) {
   $atts = shortcode_atts( array(
       'state'      => 'all',
@@ -21,10 +39,22 @@ function dracobit_generate_shortcode( $atts ) {
       'labels'     => '',
       'sort'     => 'updated',
       'direction'     => '',
-    	'repo'       => ''
+    	'repo'       => 'vragosta'
   ), $atts );
 
-  return $atts;
+  array_walk_recursive( $atts, 'sanitize_text_field' );
+
+  if ( ! empty( $atts['repo'] ) ) {
+    $milestones = dracobit_get_milestones( $atts );
+  } else {
+    return "Error: No repository found.";
+  }
+
+  foreach( $milestones as $milestone ) {
+    $output .= $milestone->title;
+  }
+
+  return $output;
 }
 add_shortcode( 'dracobit', 'dracobit_generate_shortcode' );
 
