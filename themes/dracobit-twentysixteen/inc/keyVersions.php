1d4763a743a7c2028c9402c41c620246c29cd150
Patrick Centeno <pcenteno3492@gmail.com>
2016-06-08 21:31:13 -0400

    Adding the first api key as a string

---
+++ themes/dracobit-twentysixteen/inc/keyVersions.php
@@ -0,0 +1,8 @@
+ <?php 
+ 	/**
+	 * This contains all versions of the api key in string format
+	 */
+
+ 	namespace API_KEY;
+ 	$V1 = "b01e3d1bdd11ffddfe701350243b4f055250b1300d1b84c6e49049e748d83bc9";
+  ?>
