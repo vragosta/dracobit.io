a301f7eb002094a2c15ed6de056a34fd0cdd6873
Vincent Ragosta <allchenzo@gmail.com>
2016-06-10 14:52:51 -0400

    began revamping header based on wp standards

--- themes/dracobit-twentysixteen/header.php
+++ themes/dracobit-twentysixteen/header.php
@@ -3,13 +3,10 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="initial-scale=1, width=device-width, user-scalable=no">
-		<title><?php bloginfo(); wp_title( '|', true ); ?></title>
-
-		<?php
-			if ( is_singular() || is_page( 'profile' ) ) wp_enqueue_script( 'comment-reply' );
-			wp_head();
-			$current_user = wp_get_current_user();
-		?>
+		<title><?php bloginfo(); wp_title( '|', true ); ?></title><?php
+		if ( is_singular() || is_page( 'profile' ) ) wp_enqueue_script( 'comment-reply' );
+		wp_head();
+		$current_user = wp_get_current_user(); ?>
 	</head>
 
 	<body <?php body_class(); ?>>
@@ -25,38 +22,32 @@
 		    </div>
 
 		    <div class="collapse navbar-collapse" id="dracobit-header">
-		      <ul class="nav navbar-nav">
-						<li class="<?php echo ( $_SERVER['REQUEST_URI'] == '/blog/' ) ? 'active' : '' ; ?>"><a href="<?php echo esc_url( home_url( '/blog' ) ); ?>">Home</a></li>
-						<li class="<?php echo ( $_SERVER['REQUEST_URI'] == '/tutorial/' ) ? 'active' : '' ; ?>"><a href="<?php echo esc_url( home_url( '/tutorial' ) ); ?>">Tutorials</a></li>
-						<li class="<?php echo ( $_SERVER['REQUEST_URI'] == '/chapter/' ) ? 'active' : '' ; ?>"><a href="<?php echo esc_url( home_url( '/chapter' ) ); ?>">Chapters</a></li>
-						<?php
+		      <ul class="nav navbar-nav"><?php
+						if ( dracobit_login_check() ) { ?>
+							<li class="<?php echo ( $_SERVER['REQUEST_URI'] == '/blog/' ) ? 'active' : '' ; ?>"><a href="<?php echo esc_url( home_url( '/blog' ) ); ?>">Home</a></li>
+							<li class="<?php echo ( $_SERVER['REQUEST_URI'] == '/tutorial/' ) ? 'active' : '' ; ?>"><a href="<?php echo esc_url( home_url( '/tutorial' ) ); ?>">Tutorials</a></li>
+							<li class="<?php echo ( $_SERVER['REQUEST_URI'] == '/chapter/' ) ? 'active' : '' ; ?>"><a href="<?php echo esc_url( home_url( '/chapter' ) ); ?>">Chapters</a></li><?php
 							if ( current_user_can( 'edit_posts' ) ) { ?>
 								<li><a href="<?php echo esc_url( admin_url() ); ?>">WP-Admin</a></li><?php
-							}
-
-							if ( ! is_user_logged_in() ) { ?>
-								<li class="dracobit-login">
-									<fieldset id="dracobit-login-form" class="form-inline">
-										<div class="form-group">
-											<input type="text" class="autocomplete-fix" />
-											<input name="dracobit-login-username" id="dracobit-login-username" class="form-control" placeholder="Username" type="text" aria-describedby="login-username-icon" required />
-										</div>
-										<div class="form-group">
-											<input type="text" class="autocomplete-fix" />
-											<input name="dracobit-login-password" id="dracobit-login-password" class="form-control" placeholder="Password" type="password" aria-describedby="login-password-icon" required />
-										</div>
-										<input type="hidden" name="dracobit-login-nonce" value="<?php echo wp_create_nonce( 'dracobit-login-nonce' ); ?>"/>
-										<button id="dracobit-login-submit" class="btn btn-secondary">Submit</button>
-									</fieldset>
-								</li>
-							<?php }
-
-							if ( is_user_logged_in() ) { ?>
-								<li class="<?php echo ( $_SERVER['REQUEST_URI'] == '/profile/' ) ? 'active' : '' ; ?>"><a href="<?php echo esc_url( home_url( '/profile' ) ); ?>"><?php echo esc_html( $current_user->display_name ); ?></a></li>
-								<li><a id="dracobit-logout-submit" class="btn btn-default">Logout</a></li>
-								<?php
-							}
-						?>
+							} ?>
+							<li class="<?php echo ( $_SERVER['REQUEST_URI'] == '/profile/' ) ? 'active' : '' ; ?>"><a href="<?php echo esc_url( home_url( '/profile' ) ); ?>"><?php echo esc_html( $current_user->display_name ); ?></a></li>
+							<li><a id="dracobit-logout-submit">Logout</a></li><?php
+						} else { ?>
+							<li class="dracobit-login">
+								<fieldset id="dracobit-login-form" class="form-inline">
+									<div class="form-group">
+										<!-- <input type="text" class="autocomplete-fix" /> -->
+										<input name="dracobit-login-username" id="dracobit-login-username" class="form-control" placeholder="Username" type="text" aria-describedby="login-username-icon" required />
+									</div>
+									<div class="form-group">
+										<!-- <input type="text" class="autocomplete-fix" /> -->
+										<input name="dracobit-login-password" id="dracobit-login-password" class="form-control" placeholder="Password" type="password" aria-describedby="login-password-icon" required />
+									</div>
+									<input type="hidden" name="dracobit-login-nonce" value="<?php echo wp_create_nonce( 'dracobit-login-nonce' ); ?>"/>
+									<button id="dracobit-login-submit" class="btn btn-secondary">Submit</button>
+								</fieldset>
+							</li><?php
+						} ?>
 		      </ul>
 		    </div>
 		  </div>
