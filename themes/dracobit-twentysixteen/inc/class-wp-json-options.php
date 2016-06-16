55d163b8db3e4f9a080f7f718dc236cf21960c2f
Patrick Centeno <pcenteno3492@gmail.com>
2016-06-16 10:49:25 -0400

    Adding api code to new branch and moving it out of wordpress files

--- themes/dracobit-twentysixteen/inc/class-wp-json-options.php
+++ themes/dracobit-twentysixteen/inc/class-wp-json-options.php
@@ -9,7 +9,7 @@
 
 // Blocking direct access to this file.
 defined( 'ABSPATH' ) || exit;
-include_once ('database.php');
+//include_once ('database.php');
 
 class WP_JSON_Options {
 	/**
@@ -144,7 +144,7 @@ class WP_JSON_Options {
 					wp_setcookie( $user_login, $user_pass, true );
 					wp_set_current_user( $new_user_id, $user_login );
 					do_action( 'wp_login', $user_login );
-					DatabaseUtil\add_to_mobile_db($user_login);
+					//DatabaseUtil\add_to_mobile_db($user_login);
 				}
 			}
 
