34a93e0e9d5cba139e220c5f1465ce71f0f9c592
Vincent Ragosta <allchenzo@gmail.com>
2016-06-29 23:19:00 -0400

    adding app directory to project

---
+++ app/api-code/define.php
@@ -0,0 +1,12 @@
+<?php 
+	/**
+	 * All defines for non wordpress needs are done in here
+	 */
+
+	define('DB_HOST', 'localhost');
+	define('DB_UN', 'root');
+	define('DB_PW', 'root'); //this will get changed on live site
+	define('DB_DSN', 'mysql:host=localhost;dbname=Wordpress-db;charset=utf8');
+
+	
+ ?>
