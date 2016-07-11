4614c6b0af3130d22e8279a0dcfabbdbf24f2eb6
blandvogt <brianlandvogt@gmail.com>
2016-07-11 13:28:46 -0400

    Task 5

---
+++ themes/dracobit-twentysixteen/page-brians-workarea.php
@@ -0,0 +1,82 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+crossorigin="anonymous">
+
+<?php get_header(); ?>
+
+<!-- TASK 1 -->
+<section class="container" style="height: 250px; background-color: #333;">
+</section>
+
+<!-- TASK 2 -->
+<section class="container-fluid" style="height: 200px; background-color: #ccc;">
+</section>
+
+<!-- TASK 3 -->
+<section class="row" style="height: 300px; background-color: #ddd;">
+</section>
+<section class="row-fluid" style="height: 150px; background-color: #f26531;">
+</section>
+
+<!-- TASK 4: Grid System-->
+<section class="row-fluid">
+	<!-- <div class="span1" style="height: 150px; background-color: #333">.span1 </div>
+	<div class="span1" style="height: 150px; background-color: #ccc">.span1 </div>
+	<div class="span1" style="height: 150px; background-color: #333">.span1 </div>
+	<div class="span1" style="height: 150px; background-color: #ccc">.span1 </div>
+	<div class="span1" style="height: 150px; background-color: #333">.span1 </div>
+	<div class="span1" style="height: 150px; background-color: #ccc">.span1 </div>
+	<div class="span1" style="height: 150px; background-color: #333">.span1 </div>
+	<div class="span1" style="height: 150px; background-color: #ccc">.span1 </div>
+	<div class="span1" style="height: 150px; background-color: #333">.span1 </div>
+	<div class="span1" style="height: 150px; background-color: #ccc">.span1 </div>
+	<div class="span1" style="height: 150px; background-color: #333">.span1 </div>
+	<div class="span1" style="height: 150px; background-color: #ccc">.span1 </div> -->
+
+	<!--<div class="col-xs-4 col-sm-3 col-md-2 col-lg-1" style="height: 150px; background-color: #333">.span1 </div>
+	<div class="col-xs-4 col-sm-3 col-md-2 col-lg-1" style="height: 150px; background-color: #ccc">.span1 </div>
+	<div class="col-xs-4 col-sm-3 col-md-2 col-lg-1" style="height: 150px; background-color: #333">.span1 </div>
+	<div class="col-xs-4 col-sm-3 col-md-2 col-lg-1" style="height: 150px; background-color: #ccc">.span1 </div>
+	<div class="col-xs-4 col-sm-3 col-md-2 col-lg-1" style="height: 150px; background-color: #333">.span1 </div>
+	<div class="col-xs-4 col-sm-3 col-md-2 col-lg-1" style="height: 150px; background-color: #ccc">.span1 </div>
+	<div class="col-xs-4 col-sm-3 col-md-2 col-lg-1" style="height: 150px; background-color: #333">.span1 </div>
+	<div class="col-xs-4 col-sm-3 col-md-2 col-lg-1" style="height: 150px; background-color: #ccc">.span1 </div>
+	<div class="col-xs-4 col-sm-3 col-md-2 col-lg-1" style="height: 150px; background-color: #333">.span1 </div>
+	<div class="col-xs-4 col-sm-3 col-md-2 col-lg-1" style="height: 150px; background-color: #ccc">.span1 </div>
+	<div class="col-xs-4 col-sm-3 col-md-2 col-lg-1" style="height: 150px; background-color: #333">.span1 </div>
+	<div class="col-xs-4 col-sm-3 col-md-2 col-lg-1" style="height: 150px; background-color: #ccc">.span1 </div>
+</section>
+
+	<section class="row-fluid">
+		<div class="col-xs-12 col-sm-1 col-md-6 col-lg-3" style="height: 150px; background-color: #133">.span1 </div>
+		<div class="col-xs-12 col-sm-1 col-md-6 col-lg-3" style="height: 150px; background-color: #ccc">.span1 </div>
+		<div class="col-xs-12 col-sm-1 col-md-6 col-lg-3" style="height: 150px; background-color: #133">.span1 </div>
+		<div class="col-xs-12 col-sm-1 col-md-6 col-lg-3" style="height: 150px; background-color: #ccc">.span1 </div>
+		<div class="col-xs-12 col-sm-1 col-md-6 col-lg-3" style="height: 150px; background-color: #133">.span1 </div>
+		<div class="col-xs-12 col-sm-1 col-md-6 col-lg-3" style="height: 150px; background-color: #ccc">.span1 </div>
+		<div class="col-xs-12 col-sm-1 col-md-6 col-lg-3" style="height: 150px; background-color: #133">.span1 </div>
+		<div class="col-xs-12 col-sm-1 col-md-6 col-lg-3" style="height: 150px; background-color: #ccc">.span1 </div>
+		<div class="col-xs-12 col-sm-1 col-md-6 col-lg-3" style="height: 150px; background-color: #133">.span1 </div>
+		<div class="col-xs-12 col-sm-1 col-md-6 col-lg-3" style="height: 150px; background-color: #ccc">.span1 </div>
+		<div class="col-xs-12 col-sm-1 col-md-6 col-lg-3" style="height: 150px; background-color: #133">.span1 </div>
+		<div class="col-xs-12 col-sm-1 col-md-6 col-lg-3" style="height: 150px; background-color: #ccc">.span1 </div>
+	</section>-->
+
+<!-- TASK 5 -->
+
+<div class="col-xs-4 col-sm-4 col-md-1 col-lg-1" style="height: 150px; background-color: red">.span1 </div>
+<div class="col-xs-4 col-sm-4 col-md-1 col-lg-1" style="height: 150px; background-color: red">.span1 </div>
+<div class="col-xs-4 col-sm-4 col-md-5 col-lg-1" style="height: 150px; background-color: red">.span1 </div>
+<div class="col-xs-1 col-sm-3 col-md-0 col-lg-1" style="height: 150px; background-color: black">.span1 </div>
+<div class="col-xs-1 col-sm-3 col-md-0 col-lg-1" style="height: 150px; background-color: black">.span1 </div>
+<div class="col-xs-1 col-sm-3 col-md-0 col-lg-1" style="height: 150px; background-color: black">.span1 </div>
+<div class="col-xs-1 col-sm-3 col-md-1 col-lg-1" style="height: 150px; background-color: black">.span1 </div>
+<div class="col-xs-1 col-sm-3 col-md-1 col-lg-1" style="height: 150px; background-color: black">.span1 </div>
+<div class="col-xs-1 col-sm-3 col-md-1 col-lg-1" style="height: 150px; background-color: black">.span1 </div>
+<div class="col-xs-4 col-sm-3 col-md-0 col-lg-1" style="height: 150px; background-color: blue">.span1 </div>
+<div class="col-xs-4 col-sm-3 col-md-1 col-lg-1" style="height: 150px; background-color: blue">.span1 </div>
+<div class="col-xs-4 col-sm-3 col-md-1 col-lg-1" style="height: 150px; background-color: blue">.span1 </div>
+
+
+
+<?php get_footer(); ?>
