2168577208b16fa240818316c4dc4017d364ff8f
Vincent Ragosta <vragosta@storycorps.org>
2016-07-15 09:53:31 -0400

    pushed solution to intern task one

---
+++ themes/dracobit-twentysixteen/page-solutions.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<!-- Intern Task One -->
+<section class="container" style="margin-top: 100px;">
+	<div class="row-fluid">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="border: 1px solid #333; height: 150px; background: #000;"></div>
+	</div>
+	<div class="row-fluid">
+		<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6" style="border: 1px solid #333; height: 450px; background: #fff;"></div>
+		<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6" style="border: 1px solid #333; height: 450px; background: #fff;"></div>
+	</div>
+	<div class="row-fluid">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="border: 1px solid #333; height: 150px; background: #000;"></div>
+	</div>
+</section>
+<?php get_footer(); ?>
