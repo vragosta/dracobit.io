f473aefadfca7a1a24d11dd3a5f0b605a75185b7
blandvogt <brianlandvogt@gmail.com>
2016-08-02 11:54:55 -0400

    completed intern task 4 - populate bootstrap grid (closes #191)

--- themes/dracobit-twentysixteen/page-brians-workarea.php
+++ themes/dracobit-twentysixteen/page-brians-workarea.php
@@ -174,77 +174,43 @@ crossorigin="anonymous">
 
 <!-- Intern Task Four - CSS Bootstrap Exercise #191 -->
 
-<!-- <section class="row-fluid" style="margin-top: 100px; border: 2px solid black; background-color: white">
-<div class="span2" style= "background-color: #EBEBEB; margin-top: 20px; display:flex; flex-direction:column">
-<div>
-<h4><strong>Some Favorites</strong></h4>
-<ul type="disk">
-<li> Tortor Ultricies </li>
-<li> Tellus Vestibulum </li>
-<li> Consectetur Porta </li>
-</ul>
 
-<form> <input type="text" required> <input class="button-search" type="button" value="Search" > </form>
-</div>
+ <section class="container main-box">
 
-<div class="span6;">
-<h2>Egestas Fermentum Vehicula Venenatis</h2>
-<p> Nullam quis risus eget urna mollis ornare vel eu leo. Maecenas sed diam
-eget risus varius blandit sit amet non magna. Donec id elit non mi porta
-gravida at eget metus. Sed posuere consectetur est at lobortis. Donec
-ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum
-nulla sed consectetur. Sed posuere consectetur est at lobortis.</p>
-<p> Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nullam
-id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante
-venenatis dapibus posuere velit aliquet. Fusce dapibus, tellus ac cursus
-commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
-risus.</p>
-</div>
-
-</section> -->
-
-
-<section class="container" style="margin-top: 110px; background-color:white; border: 2px solid black; display:flex; align-items: baseline">
-
-	<div class="row-fluid col-xs-2 col-sm-2 col-md-2 col-lg-2" style="background-color: #DEDDDD;
-	border-radius: 3px">
+	<div class="favorite-box col-xs-6 col-sm-3 col-md-3 col-lg-2">
 		<h4><strong>Some Favorites</strong></h4>
 		<ul type="disk">
-			<li> Tortor Ultricies </li>
-			<li> Tellus Vestibulum </li>
-			<li> Consectetur Porta </li>
+			<li> Celery Root </li>
+			<li> Spaghetti Squash </li>
+			<li> Killer Mushrooms </li>
 		</ul>
-		<form> <input type="text" required style="border-radius: 3px;"> <input class="button-search" type="button"
-			value="Search Recepies" style="background-color: #0E58A2; color:white; border-radius: 5px; border:none;
-			font-size: 12" > </form>
+		<form> <input class="search-bar" type="text" required> <input class="button-search" type="button"
+			value="Search Recepies"> </form>
 	</div>
 
-	<div class="row-fluid col-xs-10 col-sm-10 col-md-10 col-lg-10">
-
+	<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 		<div>
 			<h2><strong>Wild and Wacky Vegetables</strong></h2>
-			<blockquote> Nullam quis risus eget urna mollis ornare vel eu leo. Maecenas sed diam
-			eget risus varius blandit sit amet non magna. Donec id elit non mi porta
-			gravida at eget metus. Sed posuere consectetur est at lobortis. Donec
-			ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum
-			nulla sed consectetur. Sed posuere consectetur est at lobortis. <BR> <cite style="color:#B8B8B8">- Tom Robbins</cite> </blockquote>
+			<blockquote> The beet is the most intense of vegetable. The radish, admittedly, is more feverish,
+				but the fire of the radish is a cold fire, the fire of discontent not of passion. Tomatoes are
+				lusty enough, yet there runs through tomatoes an undercurrent of frivolity. Beets are deadly serious.
+				<BR> <cite class="quote"> - Tom Robbins </cite> </blockquote>
 		</div>
 
 		<div>
 			<h3><strong> Some Diseases</strong></h3>
-			<table border="1" width="100%" style="border-collapse:colapse; border-radius:3px;
-			border-style:hidden; box-shadow: 0 0 0 1px;">
-				<tr height="35px" >
-					<td style="padding-left:10px"><strong> Veggie </strong></td>
-					<td style="padding-left:10px"><strong> Disease </strong></td>
+			<table class="disease-table" border="1">
+				<tr>
+					<td><strong> Veggie </strong></td>
+					<td><strong> Disease </strong></td>
 				</tr>
-				<tr height="35px">
-					<td style="padding-left:10px"> Beets </td>
-					<td style="padding-left:10px"> Beeturia </td>
+				<tr>
+					<td> Beets </td>
+					<td> Beeturia </td>
 				</tr>
-				<tr height="35px">
-					<td style="padding-left:10px"> Carrots </td>
-					<td style="padding-left:10px"> Carotenosis </td>
+				<tr>
+					<td> Carrots </td>
+					<td> Carotenosis </td>
 				</tr>
 			</table>
 		</div>
@@ -278,21 +244,9 @@ risus.</p>
 			commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
 			risus.</p>
 		</div>
-
 	</div>
 
 </section>
-
-
-
-
-
-
-
-
-
-
-
 
 
 
